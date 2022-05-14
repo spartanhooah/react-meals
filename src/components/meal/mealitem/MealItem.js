950252be77479cbf5d591b52a4085058044fff22
Ben <spartanhooah@gmail.com>
2022-05-14 16:31:37 -0400

    add context and logic for adding meals

--- src/components/meal/mealitem/MealItem.js
+++ src/components/meal/mealitem/MealItem.js
@@ -1,9 +1,16 @@
+import { useContext } from "react";
+import MealsContext from "../../../store/meals-context";
 import classes from "./MealItem.module.css";
 import MealItemForm from "./MealItemForm";
 
 const MealItem = (props) => {
+  const mealContext = useContext(MealsContext);
   const meal = props.meal;
   const price = `$${meal.price.toFixed(2)}`;
+
+  const addMealHandler = (amount) => {
+    mealContext.addMeal(meal, amount);
+  };
 
   return (
     <li className={classes.meal}>
@@ -13,7 +20,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm meal={meal} />
+        <MealItemForm meal={meal} onAddMeal={addMealHandler} />
       </div>
     </li>
   );
--- src/components/meal/mealitem/MealItemForm.js
+++ src/components/meal/mealitem/MealItemForm.js
@@ -1,10 +1,12 @@
-import { useContext, useState } from "react";
+import { useContext, useRef, useState } from "react";
 import MealsContext from "../../../store/meals-context";
 import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
   const [mealCount, setMealCount] = useState(0);
+  const [isAmountValid, setIsAmountValid] = useState(true);
   const ctx = useContext(MealsContext);
+  const amountInputRef = useRef();
 
   const mealCountHandler = (event) => {
     setMealCount(event.target.value);
@@ -12,18 +14,32 @@ const MealItemForm = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    ctx.addMeal(props.meal, mealCount);
+    const enteredAmount = +amountInputRef.current.value;
+
+    if (enteredAmount < 0 || enteredAmount > 5) {
+      setIsAmountValid(false);
+
+      return;
+    }
+
+    props.onAddMeal(enteredAmount);
   };
 
   return (
     <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.input}>
-        <label htmlFor={'amount'}>Amount</label>
-        <input type="number" inputMode="numeric" onChange={mealCountHandler} />
+        <label htmlFor={"amount"}>Amount</label>
+        <input
+          ref={amountInputRef}
+          type="number"
+          inputMode="numeric"
+          onChange={mealCountHandler}
+        />
       </div>
-      <button type="submit">
-        + Add
-      </button>
+      <button type="submit">+ Add</button>
+      {!isAmountValid && (
+        <p>Please enter a valid amount for this meal (1-5).</p>
+      )}
     </form>
   );
 };
