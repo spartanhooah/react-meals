ea29bebaa3473a331f37a975c2734f2a2ec89b0a
Ben Frey <spartanhooah@gmail.com>
2022-05-16 20:34:57 -0400

    make cart button bump on adding an item

--- src/components/layout/HeaderCartButton.js
+++ src/components/layout/HeaderCartButton.js
@@ -1,17 +1,40 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../cart/CartIcon";
 import MealsContext from "../../store/meals-context";
 
 const HeaderCartButton = ({ showCart }) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const context = useContext(MealsContext);
 
-  const numberOfMeals = context.meals.reduce((currentCount, meal) => {
+  const meals = context.meals;
+
+  const numberOfMeals = meals.reduce((currentCount, meal) => {
     return currentCount + meal.count;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (meals.length === 0) {
+      return;
+    }
+
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [meals]);
+
   return (
-    <button className={classes.button} onClick={showCart}>
+    <button className={btnClasses} onClick={showCart}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
